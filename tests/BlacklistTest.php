148d09c4bca31f3fd07ce60de5d930b59cfa178b
Zander Baldwin <zander.baldwin@basekit.com>
2015-08-24 16:41:23 +0100

    Add Some Data
    The likelihood of this data even passing once is low. I haven't checked it. I'll do it later. I'm lazy.

--- tests/BlacklistTest.php
+++ tests/BlacklistTest.php
@@ -39,7 +39,16 @@ class BlacklistTest extends \PHPUnit_Framework_TestCase
     public function getListedIpAddresses()
     {
         return array(
-            #array(''),
+            // IPv4
+            '192.168.0.1/16',
+            '153.254.102.145',
+            '162.158.82.85/10',
+            '253.236.138.172/23',
+            // IPv6
+            'c35b:64f6:5005:426e:7a18:e743:9f6d:df49/100',
+            '7658:924f:200b:024c:39d4:f70f:c8f4:afe9',
+            '10de:d82a:b44b:4c83:99b1:234d:9f1a:4986/45',
+            '9869:cd0f:6ba3:78cf:4d72:41ee:d6d5:39c4/73',
         );
     }
 
@@ -52,7 +61,33 @@ class BlacklistTest extends \PHPUnit_Framework_TestCase
     public function getClientIpAddresses()
     {
         return array(
-            #array(''),
+            // IPv4
+            array('62.232.95.18'),
+            array('245.240.202.151'),
+            array('118.192.17.224'),
+            array('153.254.102.145'),
+            array('5.242.38.165'),
+            array('247.132.107.43'),
+            array('147.248.222.75'),
+            array('210.66.65.106'),
+            array('56.148.47.46'),
+            array('134.249.197.251'),
+            array('186.213.220.85'),
+            array('213.68.229.217'),
+            array('56.12.127.48'),
+            // IPv6
+            array('e68f:b206:e432:5c43:0a28:ac2e:7720:7af9'),
+            array('8861:8825:3835:d1ff:e327:4875:0fef:26c2'),
+            array('d789:c67f:6a5c:3b5a:ca74:8f3b:50b2:fadd'),
+            array('34f6:ed98:10be:d265:17ff:f062:fd70:a976'),
+            array('d7c8:9855:70ac:756e:2691:9cbb:0dc9:6856'),
+            array('43f7:5937:c939:30ef:d710:185a:2c8d:c055'),
+            array('3cf8:a9a4:99bb:2688:ec8d:fde9:25bb:70f2'),
+            array('0197:9d87:df58:866e:1d3c:bc5d:29dd:110e'),
+            array('1b2f:f066:53d9:f87c:6552:d959:ee19:f184'),
+            array('8f49:1034:e156:1b6d:924b:8485:0798:b7c8'),
+            array('e327:4875:0fef:26c2:ec8d:fde9:25bb:70f2'),
+            array('e68f:b206:e432:5c43:d710:185a:2c8d:c055'),
         );
     }
 
@@ -77,7 +112,6 @@ class BlacklistTest extends \PHPUnit_Framework_TestCase
     /**
      * Test: It Returns 403 for Non-listed IP Addresses
      *
-     * @test
      * @dataProvider getClientIpAddresses
      * @access public
      * @param string $clientIp
